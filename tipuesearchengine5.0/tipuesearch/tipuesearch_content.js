1a7b5840dfa8eb050c8890ea4d23ab89325f373a
jayangue2016 <jayangue2016@gmail.com>
2016-12-22 21:58:50 +0800

    Add files via upload

---
+++ tipuesearchengine5.0/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,23 @@
+
+var tipuesearch = {"pages": [
+
+	//Insert your tipue search data here
+    {
+		"title": " ", //The title of the page
+		"text": " ", //This where you put the description.
+		"tags": " ", //Separate the keywords via blank space
+		"url": " " //This is where you put the location of the file
+	},
+	
+	//This is the sample search data for samplearticle.html and samplesearchpage.html
+	{
+		"title": "Article",
+		"text": "This is a sample article",
+		"tags": "sample article lol",
+		"url": "samplearticle.html"
+	},
+	
+]};
+
+
+
